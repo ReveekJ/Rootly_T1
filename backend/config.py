bed9189464bdd13403545bc09833375b1db4061b
Daniil Sochnev <sochnevde09@gmail.com>
2025-10-01 10:55:06 +0500

    add rabbit

---
+++ backend/config.py
@@ -0,0 +1,17 @@
+import os
+
+import dotenv
+
+dotenv.load_dotenv()
+
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+
+RABBITMQ_USER = os.getenv("RABBITMQ_USER")
+RABBITMQ_PASS = os.getenv("RABBITMQ_PASSWORD")
+RABBITMQ_HOST = os.getenv("RABBITMQ_HOST")
+RABBITMQ_PORT = os.getenv("RABBITMQ_PORT")
+RABBITMQ_VHOST = os.getenv("RABBITMQ_VHOST")
