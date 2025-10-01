365317f5bed3c0782984d169695e499deb09627b
Daniil Sochnev <sochnevde09@gmail.com>
2025-10-01 16:34:16 +0500

    update manager

--- backend/utils/ws_manager.py
+++ backend/utils/ws_manager.py
@@ -1,4 +1,3 @@
-import uuid
 from typing import List, Dict
 
 from fastapi import WebSocket
@@ -8,14 +7,14 @@ class ConnectionManager:
     def __init__(self):
         self.__active_connections: List[Dict] = []
 
-    async def connect(self, user_id: uuid.UUID, websocket: WebSocket):
+    async def connect(self, user_id: str, websocket: WebSocket):
         await websocket.accept()
         self.__active_connections.append({"user_id": user_id, "socket": websocket})
 
-    async def disconnect(self, user_id: uuid.UUID, ws: WebSocket):
+    async def disconnect(self, user_id: str, ws: WebSocket):
         self.__active_connections.remove({"user_id": user_id, "socket": ws})
 
-    async def send_message(self, user_id: uuid.UUID, message: dict):
+    async def send_message(self, user_id: str, message: dict):
         try:
             matches = [x for x in self.__active_connections if x["user_id"] == user_id]
             for ws in matches:
