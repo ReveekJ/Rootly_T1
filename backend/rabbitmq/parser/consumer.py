9c55a5cdb8916a015f168ef6b7e448da7f20ae80
Daniil Sochnev <sochnevde09@gmail.com>
2025-10-01 22:48:05 +0500

    add db (beta)

--- backend/rabbitmq/parser/consumer.py
+++ backend/rabbitmq/parser/consumer.py
@@ -1,5 +1,9 @@
+import json
+
 from aio_pika.abc import AbstractIncomingMessage
 
+from backend.api.basic_logs.models import LogModel
+from backend.db.db_config import get_async_session
 from backend.services.logs_parser import LogsParser
 from backend.utils.ws_manager import manager
 
@@ -14,4 +18,13 @@ async def on_message_parser(message: AbstractIncomingMessage):
             'result': result
         }
     )
+
+    async with get_async_session() as session:
+        m = LogModel(
+            user_id=user_id,
+            log_analistics=json.dumps(result)
+        )
+        await session.add(m)
+        await session.commit()
+
     await message.ack()
