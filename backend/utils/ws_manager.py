52f24d7d8501ad527c28b672a8d7639ba15af7d7
Daniil Sochnev <sochnevde09@gmail.com>
2025-10-01 13:37:27 +0500

    add ws

---
+++ backend/utils/ws_manager.py
@@ -0,0 +1,27 @@
+import uuid
+from typing import List, Dict
+
+from fastapi import WebSocket
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.__active_connections: List[Dict] = []
+
+    async def connect(self, user_id: uuid.UUID, websocket: WebSocket):
+        await websocket.accept()
+        self.__active_connections.append({"user_id": user_id, "socket": websocket})
+
+    async def disconnect(self, user_id: uuid.UUID, ws: WebSocket):
+        self.__active_connections.remove({"user_id": user_id, "socket": ws})
+
+    async def send_message(self, user_id: uuid.UUID, message: dict):
+        try:
+            matches = [x for x in self.__active_connections if x["user_id"] == user_id]
+            for ws in matches:
+                await ws['socket'].send_json(message)
+        except Exception as e:
+            print(e)
+
+
+manager = ConnectionManager()
